079657f79b9f47598cc5beb7c2dc254495640509
Maruska <128466144+Marusincode@users.noreply.github.com>
2023-10-09 16:47:44 +0300

    Update delay_apriori.py

--- delay_apriori.py
+++ delay_apriori.py
@@ -1,1 +1,15 @@
-#apriori on 'delay'
+#apriori on 'run_1'
+
+import pandas as pd
+import numpy as np
+from mlxtend.preprocessing import TransactionEncoder
+from mlxtend.frequent_patterns import apriori
+from mlxtend.frequent_patterns import association_rules
+
+df = pd.read_csv('C:/Users/User/desktop/apriori/run_1.csv')
+
+te = TransactionEncoder()
+te_ary = te.fit(df).transform(df)
+df = pd.DataFrame(te_ary, columns=te.columns_)
+
+frequent_itemsets = apriori(df, min_support=0.001, use_colnames=True)
